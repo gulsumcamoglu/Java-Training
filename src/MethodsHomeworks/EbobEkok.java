d56f294c53d079ff70e6c494b5a0afbebd47ee45
gulsumcamoglu <69233551+gulsumcamoglu@users.noreply.github.com>
2023-04-11 23:46:39 +0300

    EbobEkok

---
+++ src/MethodsHomeworks/EbobEkok.java
@@ -0,0 +1,20 @@
+package MethodsHomeworks;
+import java.util.Scanner;
+public class EbobEkok {
+    public static void main(String[] args) {
+        Scanner s =  new Scanner(System.in);
+        System.out.print("First number: ");
+        int first = s.nextInt();
+        System.out.print("Second number: ");
+        int sec = s.nextInt();
+        int max = 1;
+        for (int i=1;i<=first;i++){
+
+            if (first%i==0 && sec%i==0){
+                max = i;
+            }
+        }
+        System.out.println("EBOB: "+max);
+        System.out.println("EKOK: "+ (first*sec)/max);
+    }
+}
